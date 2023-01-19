ad3c5e0d9e531337a3e90c8a6eeb0f1a4f2d60a5
Lisandro Siraveña <lisandrosiravena@gmail.com>
2023-01-19 00:39:25 -0300

    vuelta

---
+++ script.js
@@ -0,0 +1,147 @@
+const TodosLosProductos = [
+  {
+    nombre: "Samsung Galaxy A53",
+    precio: 245000,
+    caract: "4G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/s/5/s53-negro-frente_1_1.png",
+    id:1,
+    cantidad: 1,
+  },
+  {
+    nombre: "Samsung Galaxy A03",
+    precio: 300000,
+    caract: "5G",
+    img: "https://tienda.personal.com.ar/images/Samsung_Galaxy_A03_Azul_Frente_min_9ae47d8c3c.png",
+    id:2,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A22 ",
+    precio: 530000,
+    caract: "4G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/a/2/a22-gris-frente.png",
+    id:3,
+    cantidad: 1,
+  },
+  {
+    nombre: "Samsung galaxy A03 Core",
+    precio: 950000,
+    caract: "",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/a/0/a03-negro-frente_2.png",
+    id:4,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A13",
+    precio: 245000,
+    caract: "5G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/a/1/a13-negro-frente.png",
+    id:5,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A23",
+    precio: 700000,
+    caract: "4G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/a/2/a23-negro-frente.png",
+    id:6,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A04S",
+    precio: 320000,
+    caract: "5G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/cache/29ccbb5c02aec1862b4f5a57a55d0f2f/f/r/frente_14.png",
+    id:7,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A33 ",
+    precio: 530000,
+    caract: "4G",
+    img: "https://tienda.movistar.com.ar/media/catalog/product/cache/29ccbb5c02aec1862b4f5a57a55d0f2f/a/3/a33-negro-frente.png",
+    id:8,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A30",
+    precio: 900000,
+    caract: "5G",
+    img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/galaxy-a30_1187_Imagen.png",
+    id:9,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A32",
+    precio: 740000,
+    caract: "4G",
+    img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/samsung-galaxy-a32_2261348_Imagen.png",
+    id:10,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A21s",
+    precio: 335000,
+    caract: "5G",
+    img: "https://catalogo.movistar.com.pe/ArchivosUsuario/ImagenEquipo/galaxy-a21s_40064293_Big_Imagen.png",
+    cantidad: 11,
+    cantidad: 1,
+  },
+  {
+    nombre: "amsung Galaxy A51",
+    precio: 790000,
+    caract: "4G",
+    img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/galaxy-a51_1327_Imagen.png",
+    id:12,
+    cantidad: 1,
+  },
+];
+
+const contenedor = document.getElementById("contenedor");
+
+let carrito = []
+
+TodosLosProductos.forEach((producto) => {
+  const {id,nombre,cantidad,precio,img,caract} = producto
+  contenedor.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="${img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${nombre}</h5>
+    <p class="card-text">Características: ${caract}</p>
+    <p class="card-text">Cantidad: ${cantidad}</p>
+    <p class="card-text">Precio: ${precio}</p>
+    
+<button onclick="agregarProducto(${id})" class="btn btn-primary">Agregar al carrito</button>
+  </div>
+</div>`
+});
+
+
+const agregarProducto = (id) =>{
+const item = TodosLosProductos.find((producto) => producto.id === id)
+carrito.push(item)
+
+}
+
+const mostrarCarrito = () =>{
+  const modalBody = document.querySelector('.modal .modal-body')
+
+  
+  carrito.forEach((producto) => {
+const {id, nombre, img, caract, cantidad, precio} = producto
+modalBody.innerHTML +=`
+<div class="modal-contenedor">
+<div>
+<img class="img-fluid img-carrito" src="${img}"
+</div>
+
+<div>
+<p>Producto: ${nombre}</p>
+<p>Preio: ${precio}</p>
+<p>Cantidad: ${cantidad}</p>
+
+<button class="btn btn-danger">Eliminar producto</button>
+</div>
+`
+  })
+}
