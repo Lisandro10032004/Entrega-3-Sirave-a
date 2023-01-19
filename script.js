80cb248187fe453dd3cf38580fc5951e809a2f00
Lisandro Siraveña <lisandrosiravena@gmail.com>
2023-01-19 03:46:06 -0300

    proyecto casi acabado

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const TodosLosProductos = [
     precio: 245000,
     caract: "4G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/s/5/s53-negro-frente_1_1.png",
-    id:1,
+    id: 1,
     cantidad: 1,
   },
   {
@@ -12,7 +12,7 @@ const TodosLosProductos = [
     precio: 300000,
     caract: "5G",
     img: "https://tienda.personal.com.ar/images/Samsung_Galaxy_A03_Azul_Frente_min_9ae47d8c3c.png",
-    id:2,
+    id: 2,
     cantidad: 1,
   },
   {
@@ -20,15 +20,15 @@ const TodosLosProductos = [
     precio: 530000,
     caract: "4G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/a/2/a22-gris-frente.png",
-    id:3,
+    id: 3,
     cantidad: 1,
   },
   {
     nombre: "Samsung galaxy A03 Core",
     precio: 950000,
-    caract: "",
+    caract: "5G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/a/0/a03-negro-frente_2.png",
-    id:4,
+    id: 4,
     cantidad: 1,
   },
   {
@@ -36,7 +36,7 @@ const TodosLosProductos = [
     precio: 245000,
     caract: "5G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/a/1/a13-negro-frente.png",
-    id:5,
+    id: 5,
     cantidad: 1,
   },
   {
@@ -44,7 +44,7 @@ const TodosLosProductos = [
     precio: 700000,
     caract: "4G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/a/2/a23-negro-frente.png",
-    id:6,
+    id: 6,
     cantidad: 1,
   },
   {
@@ -52,7 +52,7 @@ const TodosLosProductos = [
     precio: 320000,
     caract: "5G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/cache/29ccbb5c02aec1862b4f5a57a55d0f2f/f/r/frente_14.png",
-    id:7,
+    id: 7,
     cantidad: 1,
   },
   {
@@ -60,7 +60,7 @@ const TodosLosProductos = [
     precio: 530000,
     caract: "4G",
     img: "https://tienda.movistar.com.ar/media/catalog/product/cache/29ccbb5c02aec1862b4f5a57a55d0f2f/a/3/a33-negro-frente.png",
-    id:8,
+    id: 8,
     cantidad: 1,
   },
   {
@@ -68,7 +68,7 @@ const TodosLosProductos = [
     precio: 900000,
     caract: "5G",
     img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/galaxy-a30_1187_Imagen.png",
-    id:9,
+    id: 9,
     cantidad: 1,
   },
   {
@@ -76,7 +76,7 @@ const TodosLosProductos = [
     precio: 740000,
     caract: "4G",
     img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/samsung-galaxy-a32_2261348_Imagen.png",
-    id:10,
+    id: 10,
     cantidad: 1,
   },
   {
@@ -84,52 +84,79 @@ const TodosLosProductos = [
     precio: 335000,
     caract: "5G",
     img: "https://catalogo.movistar.com.pe/ArchivosUsuario/ImagenEquipo/galaxy-a21s_40064293_Big_Imagen.png",
-    cantidad: 11,
     cantidad: 1,
+    id: 11,
   },
   {
     nombre: "amsung Galaxy A51",
     precio: 790000,
     caract: "4G",
     img: "https://catalogo.movistar.com.pe/ArchivosUsuario/EquipoCaracteristica/galaxy-a51_1327_Imagen.png",
-    id:12,
+    id: 12,
     cantidad: 1,
   },
 ];
-
+let carrito = [];
 const contenedor = document.getElementById("contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const procesarCompra = document.querySelector("#comprar");
 
-let carrito = []
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  mostrarCarrito();
+});
 
 TodosLosProductos.forEach((producto) => {
-  const {id,nombre,cantidad,precio,img,caract} = producto
+  const { id, nombre, cantidad, precio, img, caract } = producto;
   contenedor.innerHTML += `<div class="card" style="width: 18rem;">
   <img src="${img}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${nombre}</h5>
     <p class="card-text">Características: ${caract}</p>
     <p class="card-text">Cantidad: ${cantidad}</p>
-    <p class="card-text">Precio: ${precio}</p>
+    <p class="card-text">Precio: $${precio}</p>
     
 <button onclick="agregarProducto(${id})" class="btn btn-primary">Agregar al carrito</button>
   </div>
-</div>`
+</div>`;
 });
 
+procesarCompra.addEventListener('click', ()=>{
+  alert("culo")
+})
+vaciarCarrito.addEventListener("click", () => {
+  carrito.length = [];
+  mostrarCarrito();
+});
 
-const agregarProducto = (id) =>{
-const item = TodosLosProductos.find((producto) => producto.id === id)
-carrito.push(item)
+const agregarProducto = (id) => {
+  const existe = carrito.some((producto) => producto.id === id);
 
-}
+  if (existe) {
+    const producto = carrito.map((producto) => {
+      if (producto.id === id) {
+        producto.cantidad++;
+      }
+    });
+  } else {
+    const item = TodosLosProductos.find((producto) => producto.id === id);
+    carrito.push(item);
+  }
 
-const mostrarCarrito = () =>{
-  const modalBody = document.querySelector('.modal .modal-body')
+  mostrarCarrito();
+};
 
-  
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+
+  modalBody.innerHTML = "";
+
   carrito.forEach((producto) => {
-const {id, nombre, img, caract, cantidad, precio} = producto
-modalBody.innerHTML +=`
+    const { id, nombre, img, cantidad, precio } = producto;
+    modalBody.innerHTML += `
 <div class="modal-contenedor">
 <div>
 <img class="img-fluid img-carrito" src="${img}"
@@ -137,11 +164,36 @@ modalBody.innerHTML +=`
 
 <div>
 <p>Producto: ${nombre}</p>
-<p>Preio: ${precio}</p>
+<p>Precio: $${precio}</p>
 <p>Cantidad: ${cantidad}</p>
 
-<button class="btn btn-danger">Eliminar producto</button>
+<button onclick="eliminarProducto(${id})" class="btn btn-danger" >Eliminar producto</button>
 </div>
-`
-  })
-}
+</div>
+`;
+  });
+
+  if (carrito.length === 0) {
+    modalBody.innerHTML = `<p class="text-center text-danger parrafo">Carrito vacío!!</p>`;
+  }
+
+  carritoContenedor.textContent = carrito.length;
+
+  precioTotal.innerText = `$${carrito.reduce(
+    (acc, producto) => acc + producto.cantidad * producto.precio,
+    0
+  )}`;
+
+  guardarStorage();
+};
+
+const eliminarProducto = (id) => {
+  const celularId = id;
+  carrito = carrito.filter((celular) => celular.id !== celularId);
+  mostrarCarrito();
+};
+
+const guardarStorage = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
---
+++ script2.js
@@ -0,0 +1,301 @@
+const stockProductos = [
+    {
+      id: 1,
+      nombre: "Crash Bandicoot",
+      cantidad: 1,
+      desc: "Juego plataformero, niveles dificiles",
+      precio: 1200,
+      img: "img/Crash.jpg",
+    },
+    {
+      id: 2,
+      nombre: "Mortal Kombat X",
+      cantidad: 1,
+      desc: "Luchas con los mejores graficos",
+      precio: 1500,
+      img: "img/mortal.jpg",
+    },
+    {
+      id: 3,
+      nombre: "Pac Man",
+      cantidad: 1,
+      desc: "Juego plataformero, niveles basicos",
+      precio: 1570,
+      img: "img/pacman.jpg",
+    },
+    {
+      id: 4,
+      nombre: "Dragon Ball Xenoverse",
+      cantidad: 1,
+      desc: "Vive la experiencia dragon ball",
+      precio: 1000,
+      img: "img/dragonball.jpg",
+    },
+    {
+      id: 5,
+      nombre: "Naruto Ninja Storm 4",
+      cantidad: 1,
+      desc: "La historia de Naruto",
+      precio: 1200,
+      img: "img/naruto.jpg",
+    },
+    {
+      id: 6,
+      nombre: "Shingeki Final Attack",
+      cantidad: 1,
+      desc: "Eren Jeager vuelve en formato gamer...",
+      precio: 1200,
+      img: "img/shingeki.jpg",
+    },
+    {
+      id: 7,
+      nombre: "League of Legends",
+      cantidad: 1,
+      desc: "No compres esto por tu bien",
+      precio: 1400,
+      img: "img/league.jpg",
+    },
+    {
+      id: 8,
+      nombre: "Call Of Duty Warzone",
+      cantidad: 1,
+      desc: "Dispara como nunca",
+      precio: 1200,
+      img: "img/callduty.jpg",
+    },
+    {
+      id: 9,
+      nombre: "Fifa 2019",
+      cantidad: 1,
+      desc: "Juego de futbol",
+      precio: 1400,
+      img: "img/fifa.jpg",
+    },
+    {
+      id: 10,
+      nombre: "Fornite",
+      cantidad: 1,
+      desc: "Battle Royale",
+      precio: 1200,
+      img: "img/fornite.jpg",
+    },
+  ];
+  let carrito = [];
+  
+  const contenedor = document.querySelector("#contenedor");
+  const carritoContenedor = document.querySelector("#carritoContenedor");
+  const vaciarCarrito = document.querySelector("#vaciarCarrito");
+  const precioTotal = document.querySelector("#precioTotal");
+  const activarFuncion = document.querySelector("#activarFuncion");
+  const procesarCompra = document.querySelector("#procesarCompra");
+  const totalProceso = document.querySelector("#totalProceso");
+  const formulario = document.querySelector('#procesar-pago')
+  
+  if (activarFuncion) {
+    activarFuncion.addEventListener("click", procesarPedido);
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  
+    mostrarCarrito();
+    document.querySelector("#activarFuncion").click(procesarPedido);
+  });
+  if(formulario){
+    formulario.addEventListener('submit', enviarCompra)
+  }
+  
+  
+  if (vaciarCarrito) {
+    vaciarCarrito.addEventListener("click", () => {
+      carrito.length = [];
+      mostrarCarrito();
+    });
+  }
+  
+  if (procesarCompra) {
+    procesarCompra.addEventListener("click", () => {
+      if (carrito.length === 0) {
+        Swal.fire({
+          title: "¡Tu carrito está vacio!",
+          text: "Compra algo para continuar con la compra",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
+      } else {
+        location.href = "compra.html";
+      }
+    });
+  }
+  
+  stockProductos.forEach((prod) => {
+    const { id, nombre, precio, desc, img, cantidad } = prod;
+    if (contenedor) {
+      contenedor.innerHTML += `
+      <div class="card mt-3" style="width: 18rem;">
+      <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${nombre}</h5>
+        <p class="card-text">Precio: ${precio}</p>
+        <p class="card-text">Descripcion: ${desc}</p>
+        <p class="card-text">Cantidad: ${cantidad}</p>
+        <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
+      </div>
+    </div>
+      `;
+    }
+  });
+  
+  const agregarProducto = (id) => {
+    const existe = carrito.some(prod => prod.id === id)
+  
+    if(existe){
+      const prod = carrito.map(prod => {
+        if(prod.id === id){
+          prod.cantidad++
+        }
+      })
+    } else {
+      const item = stockProductos.find((prod) => prod.id === id)
+      carrito.push(item)
+    }
+    mostrarCarrito()
+  
+  };
+  
+  const mostrarCarrito = () => {
+    const modalBody = document.querySelector(".modal .modal-body");
+    if (modalBody) {
+      modalBody.innerHTML = "";
+      carrito.forEach((prod) => {
+        const { id, nombre, precio, desc, img, cantidad } = prod;
+        console.log(modalBody);
+        modalBody.innerHTML += `
+        <div class="modal-contenedor">
+          <div>
+          <img class="img-fluid img-carrito" src="${img}"/>
+          </div>
+          <div>
+          <p>Producto: ${nombre}</p>
+        <p>Precio: ${precio}</p>
+        <p>Cantidad :${cantidad}</p>
+        <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+          </div>
+        </div>
+        
+    
+        `;
+      });
+    }
+  
+    if (carrito.length === 0) {
+      console.log("Nada");
+      modalBody.innerHTML = `
+      <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
+      `;
+    } else {
+      console.log("Algo");
+    }
+    carritoContenedor.textContent = carrito.length;
+  
+    if (precioTotal) {
+      precioTotal.innerText = carrito.reduce(
+        (acc, prod) => acc + prod.cantidad * prod.precio,
+        0
+      );
+    }
+  
+    guardarStorage();
+  };
+  
+  function guardarStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
+  
+  function eliminarProducto(id) {
+    const juegoId = id;
+    carrito = carrito.filter((juego) => juego.id !== juegoId);
+    mostrarCarrito();
+  }
+  function procesarPedido() {
+    carrito.forEach((prod) => {
+      const listaCompra = document.querySelector("#lista-compra tbody");
+      const { id, nombre, precio, img, cantidad } = prod;
+      if (listaCompra) {
+        const row = document.createElement("tr");
+        row.innerHTML += `
+                <td>
+                <img class="img-fluid img-carrito" src="${img}"/>
+                </td>
+                <td>${nombre}</td>
+              <td>${precio}</td>
+              <td>${cantidad}</td>
+              <td>${precio * cantidad}</td>
+              `;
+        listaCompra.appendChild(row);
+      }
+    });
+    totalProceso.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
+  }
+  
+   function enviarCompra(e){
+     e.preventDefault()
+     const cliente = document.querySelector('#cliente').value
+     const email = document.querySelector('#correo').value
+  
+     if(email === '' || cliente == ''){
+       Swal.fire({
+         title: "¡Debes completar tu email y nombre!",
+         text: "Rellena el formulario",
+         icon: "error",
+         confirmButtonText: "Aceptar",
+     })
+   } else {
+  
+    const btn = document.getElementById('button');
+  
+  // document.getElementById('procesar-pago')
+  //  .addEventListener('submit', function(event) {
+  //    event.preventDefault();
+  
+     btn.value = 'Enviando...';
+  
+     const serviceID = 'default_service';
+     const templateID = 'template_qxwi0jn';
+  
+     emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Finalizar compra';
+        alert('Correo enviado!');
+      }, (err) => {
+        btn.value = 'Finalizar compra';
+        alert(JSON.stringify(err));
+      });
+      
+     const spinner = document.querySelector('#spinner')
+     spinner.classList.add('d-flex')
+     spinner.classList.remove('d-none')
+  
+     setTimeout(() => {
+       spinner.classList.remove('d-flex')
+       spinner.classList.add('d-none')
+       formulario.reset()
+  
+       const alertExito = document.createElement('p')
+       alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
+       alertExito.textContent = 'Compra realizada correctamente'
+       formulario.appendChild(alertExito)
+  
+       setTimeout(() => {
+         alertExito.remove()
+       }, 3000)
+  
+  
+     }, 3000)
+   }
+   localStorage.clear()
+  
+   }
